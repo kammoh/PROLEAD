aaa7675ed203d7754ad525359fe270834e891485
Kamyar Mohajerani <kammoh@gmail.com>
2024-11-07 15:10:53 -0500

    fix syntax, assuming order was a type (otherwise need to use concat)

--- tests/full/dom_indep_d1_non_flatten/dom_indep_d1_non_flatten.v
+++ tests/full/dom_indep_d1_non_flatten/dom_indep_d1_non_flatten.v
@@ -10,7 +10,7 @@ module one_domain_d1 (clk, port_a, port_b, port_c, port_r);
     AND2_X1 U0 ( .A1(port_a), .A2(port_b[0]), .ZN(i0) );
     AND2_X1 U1 ( .A1(port_a), .A2(port_b[1]), .ZN(i1) );
     XOR2_X1 U2 ( .A(i1), .B(port_r), .Z(j0) );
-    DFF_X1  U3 ( .D(j0), .CK(clk), .Q(l0), .QN() );    
+    DFF_X1  U3 ( .D(j0), .CK(clk), .Q(l0), .QN() );
     XOR2_X1 U4 ( .A(i0), .B(l0), .Z(port_c) );
 endmodule
 
@@ -21,6 +21,6 @@ module dom_indep_d1_non_flatten (clk, port_a, port_b, port_c, port_r);
     input [0:0] port_r;
     output [1:0] port_c;
 
-    one_domain_d1 dom_inst0(.clk(clk), .port_a(port_a[0]), .port_b(port_b[1:0]), .port_r(port_r[0]), .port_c(port_c[0])); 
-    one_domain_d1 dom_inst1(.clk(clk), .port_a(port_a[1]), .port_b(port_b[0:1]), .port_r(port_r[0]), .port_c(port_c[1])); 
+    one_domain_d1 dom_inst0(.clk(clk), .port_a(port_a[0]), .port_b(port_b[1:0]), .port_r(port_r[0]), .port_c(port_c[0]));
+    one_domain_d1 dom_inst1(.clk(clk), .port_a(port_a[1]), .port_b(port_b[1:0]), .port_r(port_r[0]), .port_c(port_c[1]));
 endmodule
